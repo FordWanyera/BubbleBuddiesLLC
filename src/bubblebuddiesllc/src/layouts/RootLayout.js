80e2cb77b9cf79a6dd3d0be3932588d2ab520c1c
CodyCrowl <Cody_sports@aol.com>
2023-03-29 22:36:44 -0700

    root layout

--- src/bubblebuddiesllc/src/layouts/RootLayout.js
+++ src/bubblebuddiesllc/src/layouts/RootLayout.js
@@ -4,7 +4,20 @@ import { NavLink, Outlet, Link } from 'react-router-dom'
 export default function RootLayout(){
     return(
         <div className="root-layout">
+           
+           <h1>Bubble Buddies</h1>
 
+            <div className="navLinks">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="about">About</NavLink>
+                <NavLink to="jane-hopkins">Jane Hopkins</NavLink>
+                <NavLink to="fda">FDA</NavLink>
+                <NavLink to="bavaria">Bavaria</NavLink>
+            </div>
+
+           <main>
+            <Outlet />
+           </main>
         </div>
     )
 }
